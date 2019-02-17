c5f3a570ce0492d8ed1ef3e0517d17ee8b82b2a4
Fabian Miliani <fabmil22@users.noreply.github.com>
2019-02-17 20:32:12 +0100

    subida uno reducer
    SUBIDA UNO

---
+++ redux-todo-app-base/assets/js/app.js
@@ -0,0 +1,130 @@
+(function () {
+//con esta funcion creamos nuestro store
+  const { createStore} = Redux; 
+  let store;
+
+  const todos = [
+    {
+      id: 1,
+      completed: true,
+      text:"task 1"
+    },
+    {
+      id: 2,
+      completed: true,
+      text:"task 2"
+    },
+    {
+      id: 3,
+      completed: true,
+      text:"task 3"
+    },
+  ];
+
+ const initialState = {
+    todos,
+    filter: 'ALL',
+  }
+ /*fubciones puras */ 
+  const actionAdd = (payload) => {
+    return{
+      type : 'ADD_TASK',
+      payload
+    }
+  }
+  const actionDelete = (payload) => {
+    return{
+      type : 'DELETE_TASK',
+      payload
+    }
+  }
+
+  /**cierre de funciones puras */
+  document.addEventListener("DOMContentLoaded", function(event) {
+    initApp();
+   });
+
+  const reducer = ( state , action) => {
+    switch (action.type) {
+      case 'ADD_TASK':
+        return [...state , action.payload ];
+        break;
+      case 'DELETE_TASK':
+      const id = action.payload.id;
+     
+        return  state.filter(
+          item => { return item.id !== id}
+        )
+        break;
+      case 'EDIT_TASK':
+        break;  
+      default:
+        return state;
+        break;
+    }
+
+  }
+
+  function initApp(){
+//iniciando el estado
+store = createStore(
+  reducer,
+  initialState.todos,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
+const $form = document.getElementById('form');
+$form.addEventListener('submit' , (event) => {
+  event.preventDefault();
+  const data = new FormData($form);
+  console.log(data.get('text'));
+
+   const action = actionAdd({
+      id: 33,
+      completed: true,
+      text:data.get('text')
+    });
+/*   const action = actionDelete({
+        id: 1,
+      }); */
+  store.dispatch(action);
+
+  $input = document.getElementById('new-todo');
+  $input.value = '';
+})
+
+store.subscribe(handleChange);
+render();
+
+  }
+
+  function handleChange(){
+    render();
+  }
+function render(){
+  const todos = store.getState();
+  renderTodos(todos)
+}
+
+  function renderTodos(todos){
+    const $container = document.getElementById('todo-list');
+    $container.innerHTML='';
+    let todosHtml= '';
+    todos.forEach(element => {
+      todosHtml += renderTodo(element);
+      
+    });
+    $container.innerHTML=todosHtml;
+  }
+
+  function renderTodo(todo){
+    return `
+    <li data-id="${todo.id}" class="${todo.completed}">
+    <div class="view">
+    <input  class="toggle" type="checkbox" ${todo.completed ? 'cehcked' : ''}>
+    <label>${todo.text}</label>
+    <button class="destroy"></button>
+    </div>
+    </li>` ;
+  }
+})();
